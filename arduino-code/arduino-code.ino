89268105faf31fcf3c6e63cb6543085180212fc0
Paulo Aragão <6515527+pauloaragao@users.noreply.github.com>
2021-09-15 22:08:20 -0300

    Ajuste NTC Code Com Arduino Transmissor de Dados

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -1,5 +1,6 @@
 #include "Wire.h"
 #include <DHT.h>
+#include <Thermistor.h>
 
 #define myAdress 0x08 // endereco do modulo slave que pode ser um valor de 0 a 255
 
@@ -7,10 +8,33 @@
 #define DHTPIN 8
 #define DHTTYPE DHT22   // Sensor DHT 22  (AM2302)
 
+// Analog pin used to read the NTC
+#define NTC_PIN_0               A0
+#define NTC_PIN_1               A1
+#define NTC_PIN_2               A2
+#define NTC_PIN_3               A3
+#define NTC_PIN_4               A4
+
+
 //Definicoes do sensor : pino, tipo
 DHT dht(DHTPIN, DHTTYPE);
 
+//Definicao do Termistor
 
+Thermistor thermistor_0(NTC_PIN_0);
+Thermistor thermistor_1(NTC_PIN_1);
+Thermistor thermistor_2(NTC_PIN_2);
+Thermistor thermistor_3(NTC_PIN_3);
+Thermistor thermistor_4(NTC_PIN_4);
+
+// Global temperature reading
+float temp_0;
+float temp_1;
+float temp_2;
+float temp_3;
+float temp_4;
+
+//String mensageira
 String myString = "";
 
 void setup() {
@@ -22,7 +46,7 @@ void setup() {
 
 void loop() {
   // nada para ser exexutado
-    lerDHT();
+    lerTemperatura();
     delay(2000);
 }
 
@@ -33,13 +57,20 @@ void receiveEvent() {
   Wire.write(myString.c_str());
 }
 
-void lerDHT(){
+void lerTemperatura(){
   myString = "";
   //Leitura da umidade
   float h = dht.readHumidity();
   //Leitura da temperatura (Celsius)
   float t = dht.readTemperature();
-  myString = (String) h + ";" + (String) t;
+  //Leitura Temperatura Placa Solar
+  temp_0 = thermistor_0.getTemp();   // Read temperature
+  temp_1 = thermistor_1.getTemp();   // Read temperature
+  temp_2 = thermistor_2.getTemp();   // Read temperature
+  temp_3 = thermistor_3.getTemp();   // Read temperature
+  temp_4 = thermistor_4.getTemp();   // Read temperature
+  
+  myString = (String) h + ";" + (String) t + ";" + (String) temp_0 + ";" + (String) temp_1+ ";" + (String) temp_2+ ";" + (String) temp_3+ ";" + (String) temp_4;
 
   Serial.println(myString);
   return myString;
